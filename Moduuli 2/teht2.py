0fde11f3f994dce47904eeb1be123542225d1c69
Eveliina <eweeeijj@gmail.com>
2026-09-04 18:37:19 +0300

    kokeilu

---
+++ Moduuli 2/teht2.py
@@ -0,0 +1,8 @@
+import math
+
+säde = float(input("Anna ympyrän säde: "))
+
+pintaala = math.pi* säde ** 2 
+
+print("Ympyrän pinta-ala on " , pintaala)
+
---
+++ Moduuli 2/teht4.py
@@ -0,0 +1,14 @@
+import math
+luku1 = int(input("Anna kokonaisluku: "))
+luku2 = int(input("Anna toinen kokonaisluku: "))
+
+summa = (luku1 + luku2)
+tulo = (luku1 * luku2)
+keskiarvo = (luku1 + luku2) /2
+
+
+print("Lukujen summa on " , summa)
+
+print("Lukujen tulo on ", tulo)
+
+print("Lukujen keskiarvo on ", keskiarvo)
