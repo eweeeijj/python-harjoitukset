9490b629ece03765bc562c397666fa71d0f72c0f
Eveliina <eweeeijj@gmail.com>
2026-09-08 19:20:35 +0300

    vienti

---
+++ Moduuli 2/teht6.py
@@ -0,0 +1,17 @@
+import random
+luku1 = random.randint(0,9)
+luku2 = random.randint(0,9)
+luku3 = random.randint(0,9)
+
+koodi = f"{luku1}{luku2}{luku3}"
+
+print(f"Arvottu kolmenumeroinen koodi on: {koodi}")
+
+import random
+luku1 = random.randint(1,6)
+luku2 = random.randint(1,6)
+luku3 = random.randint(1,6)
+luku4 = random.randint(1,6)
+
+koodi =f"{luku1}{luku2}{luku3}{luku4}"
+print(f"Arvottu nelinumeroinen koodi on: {koodi}")
--- readme.md
+++ readme.md
@@ -6,4 +6,4 @@
 Tein tehtävät 1 ja 2
 
 ## Moduuli 2 
-
+Sain tehtyä tehtävät 1-6. Aluksi tuskailin viimeisten kolmen tehtävän kanssa, mutta onneksi ystäväni piti minulle tukiopetusta ja jotain loksahti päässä kohilleen! :)
